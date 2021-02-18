5b4af9f0753dc77146bea8ce955757fe61ee83ad
Ranjan <sranjansharma2001@gmail.com>
2021-02-18 15:44:45 +0530

    Initial commit

---
+++ app/src/main/java/com/www/roto/nav/rotonav/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.www.roto.nav.rotonav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
