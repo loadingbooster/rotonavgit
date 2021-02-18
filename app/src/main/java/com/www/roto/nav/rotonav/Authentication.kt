01bcc2891252f4dae5eb90846a2ede4af504db00
HP <panhimanshu147@gmail.com>
2021-02-19 00:19:09 +0530

    login id screen

---
+++ app/src/main/java/com/www/roto/nav/rotonav/Authentication.kt
@@ -0,0 +1,11 @@
+package com.www.roto.nav.rotonav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Authentication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
