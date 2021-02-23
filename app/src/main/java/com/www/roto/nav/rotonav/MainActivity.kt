c7c7ef218cea4a017b529e08ffbf3f72a737c49a
HP <panhimanshu147@gmail.com>
2021-02-24 00:07:29 +0530

    Splash Screen code added

--- app/src/main/java/com/www/roto/nav/rotonav/MainActivity.kt
+++ app/src/main/java/com/www/roto/nav/rotonav/MainActivity.kt
@@ -2,10 +2,29 @@ package com.www.roto.nav.rotonav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+import android.view.WindowManager
 
+
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },1000)
     }
+
 }
+
+
