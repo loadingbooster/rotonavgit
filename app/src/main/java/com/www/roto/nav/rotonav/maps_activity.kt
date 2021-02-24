07834865f5b4072ee6f3aac2606223a1bb102146
Ranjan Sharma <sranjansharam2001@gmail.com>
2021-02-25 00:45:38 +0530

    changes to firebase database

--- app/src/main/java/com/www/roto/nav/rotonav/maps_activity.kt
+++ app/src/main/java/com/www/roto/nav/rotonav/maps_activity.kt
@@ -1,24 +1,28 @@
 package com.www.roto.nav.rotonav
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.*
+
 
 class maps_activity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps_activity)
+
+        database = FirebaseDatabase.getInstance().getReference()
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
